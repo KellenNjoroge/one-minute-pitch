83b2b89652c626eb2037ab2e03d7e806cd454a7c
KellenNjoroge <muthonkel@gmail.com>
2018-09-07 15:32:45 +0300

    imported jwt

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from flask_uploads import UploadSet, configure_uploads, IMAGES
-# from flask_mail import Mail
-# from flask_simplemde import SimpleMDE
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 from config import config_options
 
 login_manager = LoginManager()
@@ -15,9 +15,9 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
-# mail = Mail()
-# photos = UploadSet('photos',IMAGES)
-# simple = SimpleMDE()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -29,18 +29,18 @@ def create_app(config_name):
     # initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-    # mail.init_app(app)
+    mail.init_app(app)
     login_manager.init_app(app)
-    # simple.init_app(app)
+    simple.init_app(app)
 
     # Registering the main app Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # Registering the auth blueprint
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-    # configure_uploads(app,photos)
+    configure_uploads(app,photos)
 
     return app
