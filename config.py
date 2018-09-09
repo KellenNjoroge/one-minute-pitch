4b948ea0afac2a3cb322522a19dbf680339f8724
KellenNjoroge <muthonkel@gmail.com>
2018-09-09 19:20:15 +0300

    added mail username and password

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kellen:Kellen@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
