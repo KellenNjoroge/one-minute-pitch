c9511b225310ae76e5257cd9e1d3cfbb8f6f0e9b
KellenNjoroge <muthonkel@gmail.com>
2018-09-09 18:47:07 +0300

    we're kinda connected

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,11 @@
-from flask import render_template, request, redirect, url_for, flash, abort
+from flask import render_template, redirect, url_for, flash, abort
 from . import main
 from ..models import User, Pitch, Comment, UpVote, DownVote
 from flask_login import login_required, current_user
 from .. import db, photos
 from .forms import PitchForm, CommentForm, UpdateProfile
 
-
+#removed request
 
 @main.route('/')
 def index():
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export SECRET_KEY=muthoni
+python3.6 manage.py server
