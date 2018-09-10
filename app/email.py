d9cc7c959911865ea1779c6043277c892c3d7fae
KellenNjoroge <muthonkel@gmail.com>
2018-09-10 11:12:07 +0300

    commented out somethings

--- app/email.py
+++ app/email.py
@@ -14,15 +14,15 @@ def mail_message(subject, template, to, **kwargs):
     mail.send(email)
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
-
-
-def send_reset_email(user):
-    token = user.get_reset_password_token()
-    send_email('Reset Password', sender=app.config['MAIL_USERNAME'], recipients=[user.email],
-               text_body=render_template('auth/reset_password.txt', user=user, token=token),
-               html_body=render_template('auth/reset_password.html', user=user, token=token))
+# def send_email(subject, sender, recipients, text_body, html_body):
+#     msg = Message(subject, sender=sender, recipients=recipients)
+#     msg.body = text_body
+#     msg.html = html_body
+#     mail.send(msg)
+#
+#
+# def send_reset_email(user):
+#     token = user.get_reset_password_token()
+#     send_email('Reset Password', sender=app.config['MAIL_USERNAME'], recipients=[user.email],
+#                text_body=render_template('auth/reset_password.txt', user=user, token=token),
+#                html_body=render_template('auth/reset_password.html', user=user, token=token))
