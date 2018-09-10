1bd224f39bf7a0fa423d60ee27af7e19d44d9c89
KellenNjoroge <muthonkel@gmail.com>
2018-09-10 11:32:53 +0300

    removed send mail defining

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from ..models import User
-from ..email import send_email, send_reset_email, mail_message
+from ..email import  mail_message
 from . import auth
 from flask_login import login_user, login_required, logout_user, current_user
 from .. import db
@@ -35,8 +35,8 @@ def register():
         db.session.commit()
 
         mail_message('Welcome to one minute pitch', 'email/welcome_user', user.email, user=user)
-        send_email(subject="Registration", sender=os.environ.get('MAIL_USERNAME'), recipients=[user.email],
-                   text_body='Test Email', html_body=render_template('500.html'))
+        # send_email(subject="Registration", sender=os.environ.get('MAIL_USERNAME'), recipients=[user.email],
+        #            text_body='Test Email', html_body=render_template('500.html'))
 
         return redirect(url_for('auth.login'))
 
